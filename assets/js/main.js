4bbfb073e65d545aa6b8e338f952a1afe41d2b48
Hans Bonini <hans@ysolutions.com.br>
2020-07-31 19:01:19 -0300

    new layout menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $(function () {
   // Sidebar toggle behavior
-  $("#exibur-menu").on("click", function () {
+  $(".exibir-menu").on("click", function () {
     $("#sidebar, #content").toggleClass("active");
   });
 });
