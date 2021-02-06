bb5f91a91e49c60c0df27288f53392997da6c401
Hans Bonini <hans@ysolutions.com.br>
2021-02-06 18:24:00 -0300

    Adicionando a busca do sistema com Typeahead.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,19 +5,56 @@ $(function () {
   });
 
   var traducoes = new Bloodhound({
-    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace("title"),
     queryTokenizer: Bloodhound.tokenizers.whitespace,
-    prefetch: '/api/traducoes.json',
+    prefetch: "/api/traducoes.json",
   });
-  
-  $('.typeahead').typeahead(null, {
-    name: 'pesquisa',
+
+  var romhacks = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace("title"),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: "/api/romhacks.json",
+  });
+
+  var tutoriais = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace("title"),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: "/api/tutoriais.json",
+  });
+
+  var utilitarios = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace("title"),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: "/api/utilitarios.json",
+  });
+
+  $(".typeahead").typeahead(null, {
+    name: "traducoes",
     source: traducoes,
-    display: 'title',
+    display: "title",
     templates: {
-      header: '<h3 class="pesquisa-resultado-tipo">Traduções</h3>'
-    }
+      header: '<h3 class="pesquisa-resultado-tipo">Traduções</h3>',
+    },
+  },{
+    name: "romhacks",
+    source: romhacks,
+    display: "title",
+    templates: {
+      header: '<h3 class="pesquisa-resultado-tipo">Romacks</h3>',
+    },
+  },{
+    name: "tutoriais",
+    source: tutoriais,
+    display: "title",
+    templates: {
+      header: '<h3 class="pesquisa-resultado-tipo">Tutoriais</h3>',
+    },
+  },{
+    name: "utilitarios",
+    source: utilitarios,
+    display: "title",
+    templates: {
+      header: '<h3 class="pesquisa-resultado-tipo">Utilitários</h3>',
+    },
   });
-
-
 });
