cb14d689984b14615aa3069343222ba9f7c3f686
Hans Bonini <hans@ysolutions.com.br>
2021-02-06 15:56:42 -0300

    Adicionando a busca do sistema com Typeahead.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,18 @@ $(function () {
   $(".exibir-menu").on("click", function () {
     $("#sidebar, #content").toggleClass("active");
   });
+
+  var pesquisa = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: '/api/pesquisa.json',
+  });
+  
+  $('#pesquisa').typeahead(null, {
+    name: 'pesquisa',
+    display: 'title',
+    source: pesquisa
+  });
+
+
 });
