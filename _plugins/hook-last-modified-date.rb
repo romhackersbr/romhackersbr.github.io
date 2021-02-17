a3f288a2f061c66cabfd50216c245983848c7046
Hans Bonini <hans@ysolutions.com.br>
2021-02-16 23:24:14 -0300

    Ajuste dos utilitários e tutoriais recentes

--- _plugins/hook-last-modified-date.rb
+++ _plugins/hook-last-modified-date.rb
@@ -1,13 +1,7 @@
-Jekyll::Hooks.register :site, :post_read do |site|
+Jekyll::Hooks.register :documents, :pre_render do |doc|
+    # get the current post last modified time
+    modification_time = File.mtime( doc.path )
 
-    site.collections.each do collection
-        collection.each do item
-            # get the current post last modified time
-            modification_time = File.mtime( item.path )
-
-            # inject modification_time in post's datas.
-            item.data['last_modified'] = modification_time
-        end
-    end
-
+    # inject modification_time in post's datas.
+    doc.data['last_modified'] = modification_time
 end
