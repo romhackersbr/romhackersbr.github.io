458358512db1d1ec79f1a1fcdbcb8ba0be9917f3
Hans Bonini <hans@ysolutions.com.br>
2021-03-01 21:46:24 -0300

    Banco de Dados sincronizado em 01/03/2021 às 21:46:23

--- sincronizar_banco.sh
+++ sincronizar_banco.sh
@@ -2,5 +2,5 @@ date=$(date +%d/%m/%Y)
 hour=$(date +%H:%M:%S)
 git submodule update --remote --merge
 git add .
-git commit -m "Banco de Dados Sincronizado $date às $hour"
+git commit -m "Banco de Dados sincronizado em $date às $hour"
 git push origin main
