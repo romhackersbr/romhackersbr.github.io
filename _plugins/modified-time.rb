957b9da02e2aa354abc7ed6a1a384ee714180b2c
Hans Bonini <hans@ysolutions.com.br>
2021-10-20 17:37:53 -0300

    Banco de Dados sincronizado em 20/10/2021 às 17:37:52

--- _plugins/modified-time.rb
+++ _plugins/modified-time.rb
@@ -9,30 +9,30 @@ module Jekyll
         MTIME_PAGE_VAR = 'last_modified'
 
         def self.get_mtime(path=nil)
-            # additional_status_options = ''
-            # additional_log_options = ''
+            additional_status_options = ''
+            additional_log_options = ''
 
-            # if (path != nil)
-            #     additional_status_options = "--ignored -- #{path}"
-            #     additional_log_options = "-- #{path}"
-            # end
+            if (path != nil)
+                additional_status_options = "--ignored -- #{path}"
+                additional_log_options = "-- #{path}"
+            end
 
-            # git_status_str = `git status --porcelain -uall #{additional_status_options}`
-            # if ($? != 0)
-            #     raise GitError, "git status failed"
-            # end
+            git_status_str = `git status --porcelain -uall #{additional_status_options}`
+            if ($? != 0)
+                raise GitError, "git status failed"
+            end
 
-            # if (!git_status_str.empty?)
-            #     # git status reported that the file or workspace has been modified/ignored/staged/...
-            #     return nil
-            # else
-            #     git_log_str = `git log -1 --format=format:"%ct" #{additional_log_options}`
-            #     if ($? != 0 || git_log_str.empty?)
-            #         raise GitError, "git log failed"
-            #     end
+            if (!git_status_str.empty?)
+                # git status reported that the file or workspace has been modified/ignored/staged/...
+                return nil
+            else
+                git_log_str = `git log -1 --format=format:"%ct" #{additional_log_options}`
+                if ($? != 0 || git_log_str.empty?)
+                    raise GitError, "git log failed"
+                end
 
-            #     return Time.at git_log_str.to_i
-            # end
+                return Time.at git_log_str.to_i
+            end
         end
 
         Jekyll::Hooks.register(:documents, :post_init, priority: LOW_PRIORITY) do |doc|
